f13eeabd3e6e9599fd1ec7bb182950c87b7e8b98
Kevin Genus <kevin.genus@gmail.com>
2017-07-25 17:21:08 -0400

    feature4: add review model attributes

---
+++ server/db/models/review.js
@@ -0,0 +1,29 @@
+'use strict'
+const Sequelize = require('sequelize')
+const db = require('../index.js')
+
+const Review = db.define('reviews', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.TEXT,
+    defaultValue: 'Please Enter A Review',
+    allowNull: false
+  },
+  rating: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    max: 10,
+    min: 0,
+    set (val) {
+      this.setDataValue('rating', val * 2)
+    },
+    get () {
+      return this.getDataValue('rating') / 2
+    }
+  }
+})
+
+module.exports = Review
