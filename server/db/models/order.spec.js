504f8e839f46ed70fc9b9c0eeed694b91c8fa6dc
kyusulamita <sulamitamorales@yahoo.com>
2017-07-27 04:57:13 -0400

    -boilerplate

---
+++ server/db/models/order.spec.js
@@ -0,0 +1,37 @@
+const {expect} = require('chai')
+const db = require('../db')
+const Order = db.model('order')
+
+describe('Ordermodel', () => {
+
+  beforeEach(() => {
+    return db.sync({force: true})
+  });
+
+  describe('Initial tests', () => {
+
+    describe('initial status', () => {
+
+      let sillyOrder
+
+      beforeEach(() => {
+        return Order.create({
+          status: 'CREATED'
+        })
+          .then(order => {
+            sillyOrder = order
+          })
+      })
+
+      it('will have status of created', () => {
+        expect(sillyOrder.getDataValue('status')).to.be.equal('CREATED')
+      })
+
+      xit('', () => {
+        expect(sillyOrder.correctPassword('bonez')).to.be.equal(false)
+      })
+
+    })
+
+  })
+})
