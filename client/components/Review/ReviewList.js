3ae496a24a3be4184a5991f6b31d0cd3629721ec
Min Hwangbo <min.hwangbo@gmail.com>
2017-07-29 16:57:01 -0400

    feature: retrieve reviews based on product id and submit reviews to the product

--- client/components/Review/ReviewList.js
+++ client/components/Review/ReviewList.js
@@ -1,49 +1,56 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { addReview } from '../store/review'
+import { fetchReviews, addReview } from '../../store/review'
+import { fetchProduct, fetchProducts } from '../../store/product'
 import ReviewItem from './ReviewItem'
-import ProductItem from '../Product/ProductItem'
 
 /* -----------------    COMPONENT     ------------------ */
 
-<<<<<<< HEAD
-=======
-// dummyData
-let testReviews = [
-  {
-    id: 1,
-    title: 'Test Review 1',
-    content: 'This is the content for Review 1',
-    rating: 4
-  },
-  {
-    id: 2,
-    title: 'Test Review 2',
-    content: 'This is the content for Review 2',
-    rating: 2
-  }
-]
-
->>>>>>> 52c47780d68509a710957079f4ffb89fec68fb7a
-export class ReviewList extends Component {
+class ReviewList extends Component {
   constructor (props) {
     super(props)
 
+    this.state = {
+      product: {
+        id: ''
+      },
+      review: {
+        title: '',
+        content: '',
+        rating: 0
+      }
+    }
+
     this.renderReviewForm = this.renderReviewForm.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
-    this.filterReview = this.filterReview.bind(this)
+  }
+
+  componentDidMount () {
+    this.props.fetchReviewsData()
+    this.props.fetchProductsData()
+  }
+
+  componentWillReceiveProps (newProps) {
+
+    if (newProps.match.params.id !== this.props.match.params.id) {
+      this.props.fetchProductsData()
+    }
+    this.setState({
+      product: newProps.retrievedProduct
+    })
   }
 
   render () {
-    const { products } = this.props
+    const { product } = this.state
+    console.log(this.props.review)
     return (
       <div className="container">
         <ul className="list-group">
           {this.renderReviewForm()}
           {
-            this.props.reviews
-              .filter(review => review.productId === products.id)
+            this.props.review
+              .filter(filteredReview => filteredReview.productId === product.id)
               .map(review => <ReviewItem review={review} key={review.id} />)
           }
         </ul>
@@ -60,13 +67,18 @@ export class ReviewList extends Component {
               name="title"
               type="text"
               placeholder="Review Title"
+              onChange={ event => this.setState({review: {title: event.target.value}}) }
             />
             <input
               name="content"
               type="text"
               placeholder="Review Content"
+              onChange={ event => this.setState({review: {content: event.target.value}}) }
             />
-            <select name="rating" defaultValue="" required>
+            <select name="rating"
+            defaultValue=""
+            onChange={ event => this.setState({review: {rating: event.target.value}}) }
+            required>
               <option value="" disabled>(Select a Rating)</option>
               <option key='1' value="1">1</option>
               <option key='2' value="2">2</option>
@@ -78,18 +90,21 @@ export class ReviewList extends Component {
           type="submit"
           className="btn btn-warning btn-xs">
           <span className="glyphicon glyphicon-plus" />
+          Submit Review
         </button>
       </form>
     )
   }
 
   onSubmit (event) {
-    event.preventDefault()
+    console.log(event.target.title.value)
     const review = {
       title: event.target.title.value,
       content: event.target.content.value,
-      rating: event.target.rating.value
+      rating: event.target.rating.value,
+      productId: this.state.product.id
     }
+    this.props.addReviewData(review)
     event.target.title.value = ''
     event.target.content.value = ''
     event.target.rating.value = ''
@@ -98,8 +113,28 @@ export class ReviewList extends Component {
 
 /* -----------------    CONTAINER     ------------------ */
 
-const mapState = ({ products, reviews }) => ({ products, reviews })
+const mapState = ({ product, review }, ownProps) => {
+  const retrievedProduct = product.find(aProduct => aProduct.id === +ownProps.match.params.id)
 
-const mapDispatch = { addReview }
+  return { retrievedProduct, review }
+}
+
+const mapDispatch = (dispatch, ownProps) => {
+  return {
+    fetchReviewsData: () => {
+      dispatch(fetchReviews())
+    },
+    fetchProductData: (ownProps) => {
+      const productId = ownProps.match.params.id
+      dispatch(fetchProduct(productId))
+    },
+    fetchProductsData: () => {
+      dispatch(fetchProducts())
+    },
+    addReviewData: (newReview) => {
+      dispatch(addReview(newReview))
+    }
+  }
+}
 
 export default connect(mapState, mapDispatch)(ReviewList)
