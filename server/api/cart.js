72e52cfdeb0cc803554c21ad231055da33b668c6
Kevin Genus <kevin.genus@gmail.com>
2017-07-31 18:59:54 -0400

    Initial commit

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,33 +2,29 @@ const router = require('express').Router()
 const {List, OrderItem, Product} = require('../db/models')
 module.exports = router
 
-
-
 router.get('/', (req, res, next) => {
   res.json(req.session.cart || {total: 0, items: []})
 })
 
-
 router.put('/addItem', (req, res, next) => {
-  let cart = req.session.cart || { total: 0, items: []} 
+  let cart = req.session.cart || {total: 0, items: []}
   let found = false
   const itemToAdd = req.body
   cart.items.map(item => {
-    if (item.productId === itemToAdd.productId){
+    if (item.productId === itemToAdd.productId) {
       found = true
       cart.total += (itemToAdd.quantity - item.quantity) * item.price
       return itemToAdd
     }
   })
-  if(!found){
+  if (!found) {
     cart.total += itemToAdd.price * itemToAdd.quantity
     cart.items.push(itemToAdd)
   }
   res.json(cart)
 })
 
-
-router.delete('/', (req,res,next) => {
+router.delete('/', (req, res, next) => {
   req.session.cart.destroy()
 
 })
