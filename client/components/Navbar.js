44cbbd96cca001420141d5cad121d7340359155d
Fara Woolf <fbwoolf@gmail.com>
2017-07-31 17:31:05 -0500

    feature: finish Navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,47 +1,90 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link, NavLink } from 'react-router-dom'
+import {logout} from '../store'
 
 /* -----------------    COMPONENT     ------------------ */
 
-const Navbar = (props) => {
-  return (
-    <nav className='navbar'>
-      <div className='container'>
-        <div className='navbar-header'>
-          <button
-            type='button'
-            className='navbar-toggle collapsed'
-            data-toggle='collapse'
-            data-target='.navbar-collapse'>
-            <span className='icon-bar' />
-            <span className='icon-bar' />
-            <span className='icon-bar' />
-          </button>
-          <ul className='nav navbar-nav navbar-left'>
-            <li>
-              <NavLink to='/home' activeClassName='active'>Vinylshop</NavLink>
-            </li>
-          </ul>
+class Navbar extends Component {
+  constructor (props) {
+    super(props)
+    this.renderLoggedIn = this.renderLoggedIn.bind(this)
+    this.renderLoggedOut = this.renderLoggedOut.bind(this)
+  }
+
+  render () {
+    const {isLoggedIn} = this.props
+
+    return (
+      <nav className='navbar'>
+        <div className='container'>
+          <div className='navbar-header'>
+            <button
+              type='button'
+              className='navbar-toggle collapsed'
+              data-toggle='collapse'
+              data-target='.navbar-collapse'>
+              <span className='icon-bar' />
+              <span className='icon-bar' />
+              <span className='icon-bar' />
+            </button>
+            <ul className='nav navbar-nav navbar-left'>
+              <li>
+                <NavLink to='/home' activeClassName='active'>Vinylshop</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className='collapse navbar-collapse'>
+            {
+            isLoggedIn ? this.renderLoggedIn() : this.renderLoggedOut()
+            }
+          </div>
         </div>
-        <div className='collapse navbar-collapse'>
-          <ul className='nav navbar-nav navbar-right'>
-            <li>
-              <NavLink to='/signup' activeClassName='active'>Signup</NavLink>
-            </li>
-            <li>
-              <NavLink to='/login' activeClassName='active'>login</NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    )
+  }
+
+  renderLoggedIn () {
+    return (
+      <ul className='nav navbar-nav navbar-right'>
+        <li>
+          <NavLink to='/home' activeClassName='active'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/login' onClick={this.props.handleClick} activeClassName='active'>Logout</NavLink>
+        </li>
+      </ul>
+    )
+  }
+
+  renderLoggedOut () {
+    return (
+      <ul className='nav navbar-nav navbar-right'>
+        <li>
+          <NavLink to='/signup' activeClassName='active'>Signup</NavLink>
+        </li>
+        <li>
+          <NavLink to='/login' activeClassName='active'>login</NavLink>
+        </li>
+      </ul>
+    )
+  }
 }
 
 /* -----------------    CONTAINER     ------------------ */
 
-const mapState = null
-const mapDispatch = null
+const mapState = (state) => {
+  return {
+    isLoggedIn: !!state.currentUser.id
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    handleClick () {
+      dispatch(logout())
+    }
+  }
+}
 
 export default connect(mapState, mapDispatch)(Navbar)
